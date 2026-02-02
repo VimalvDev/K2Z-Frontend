6875122467d4d903988b6355c4eb21690785a501
Vimal | Frontend <165493811+VimalvDev@users.noreply.github.com>
2026-02-02 12:11:59 +0530

    Remove unfinished code, and make the main branch clean

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,11 @@
 import React from "react";
 import { Link, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import Projects from "./pages/Projects";
-import Agence from "./pages/Agence";
 
 function App() {
   return (
     <>
-      <div className="loader"></div>
 
       <div className="data_container" >
-        <div className="header"></div>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/agence" element={<Agence />} />
-        </Routes>
       </div>
     </>
   );
--- src/pages/Agence.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Agence() {
-  return (
-    <div>Agence</div>
-  )
-}
-
-export default Agence
